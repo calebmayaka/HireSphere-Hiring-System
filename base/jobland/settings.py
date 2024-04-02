bc4ae831dd3fe0e2c265c342f764b608e9506e01
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-04-02 02:58:43 +0100

    update

--- base/jobland/settings.py
+++ base/jobland/settings.py
@@ -159,11 +159,17 @@ MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Email settings fot SMTP server
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'ombogomayaka@gmail.com'
-EMAIL_HOST_PASSWORD = "callycalex"
-EMAIL_USE_TLS = True  # for security
 
+# for local server
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# for online server
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = '587'
+# EMAIL_HOST_USER = 'ombogomayaka@gmail.com'
+# EMAIL_HOST_PASSWORD = "callycalex"
+# EMAIL_USE_TLS = True  # for security
+
+# p.k handling in case of undefined p.k
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
